36d7e3dcc4e554950dee2431537fa208be0f29a6
HassanFayyaz <hassanfayyaz440@gmail.com>
2021-04-06 22:55:02 +0500

    Bugs

--- src/main/java/com/example/TucShopBackend/Repositories/RequestForProductRepository.java
+++ src/main/java/com/example/TucShopBackend/Repositories/RequestForProductRepository.java
@@ -17,7 +17,7 @@ import java.util.List;
 @Repository
 public interface RequestForProductRepository extends JpaRepository<RequestForProduct,Long> {
 
-    @Query(value = "select id,count,date1,name from tucshop.request_for_product order by count desc limit 5",nativeQuery = true)
+    @Query(value = "select id,count,date1,name from foodorderingdb.request_for_product order by count desc limit 5",nativeQuery = true)
     public List<RequestForProduct> topRequestedProductsCount();
 
 
@@ -28,7 +28,7 @@ public interface RequestForProductRepository extends JpaRepository<RequestForPro
 //    @Query(value = "select new com.example.TucShopBackend.DTO.RequestForProductDTO(id, name as name,count ,date1) from RequestForProduct where name LIKE %:keyword% group by name order by count desc")
 //    public List<RequestForProductDTO> topRequestedProductsKeyword(@Param("keyword")String keyword);
 
-    @Query(value = "SELECT *  from tucshop.request_for_product WHERE name LIKE %:keyword%", nativeQuery = true)
+    @Query(value = "SELECT *  from foodorderingdb.request_for_product WHERE name LIKE %:keyword%", nativeQuery = true)
     public List<RequestForProduct> topRequestedProductsKeyword(@Param("keyword")String keyword);
 
 //    @Query(value = "select* from tucshop.request_for_product where name=:name",nativeQuery = true)
